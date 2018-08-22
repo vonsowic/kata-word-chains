429c211d511bda2ba58d7b59f9f630605d2849c7
sawim <mwasowicz7@gmail.com>
2018-08-22 16:02:17 +0200

    add methods for adding words to solver's dictionary and resolving the shortest chain of words

--- src/main/java/com/vonsowic/kata/katasolver/KataSolver.java
+++ src/main/java/com/vonsowic/kata/katasolver/KataSolver.java
@@ -1,18 +1,42 @@
 package com.vonsowic.kata.katasolver;
 
+import com.vonsowic.kata.exceptions.UnknownWordException;
+import com.vonsowic.kata.exceptions.WordsLengthNotEqualException;
+
 import java.util.Collection;
+import java.util.HashMap;
 
 public class KataSolver {
 
+    private final HashMap<Integer, WordChainGraph> graphs = new HashMap<>();
+
     public Collection<String> findChain(String startWord, String endWord) {
-        return null;
+        if (startWord == null || endWord == null) {
+            throw new UnknownWordException("Arguments must not be null");
+        }
+
+        if(startWord.length() != endWord.length()) {
+            throw new WordsLengthNotEqualException();
+        }
+
+        if ( !this.graphs.containsKey(startWord.length())) {
+            throw new UnknownWordException();
+        }
+
+
+        return this.graphs.get(startWord.length())
+                .findShortestSolution(startWord, endWord);
     }
 
     public void addWords(Collection<String> words) {
-
+        words.forEach(this::addWord);
     }
 
     public void addWord(String word) {
+        if ( !this.graphs.containsKey(word.length()) ) {
+            this.graphs.put(word.length(), new WordChainGraph());
+        }
 
+        graphs.get(word.length()).addWord(word);
     }
 }
---
+++ src/main/java/com/vonsowic/kata/katasolver/WordChainGraph.java
@@ -0,0 +1,111 @@
+package com.vonsowic.kata.katasolver;
+
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
+import com.vonsowic.kata.exceptions.NoSolutionFoundException;
+import com.vonsowic.kata.exceptions.UnknownWordException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class WordChainGraph {
+
+    private final MutableGraph<String> graph = GraphBuilder
+            .undirected()
+            .build();
+
+
+    void addWord(String newWord) {
+        this.graph.addNode(newWord);
+
+        this.graph.nodes()
+                .stream()
+                .filter(word -> shouldBeLinked(word, newWord))
+                .forEach(word -> this.graph.putEdge(word, newWord));
+    }
+
+
+    private boolean shouldBeLinked(String word1, String word2) {
+        if(word1.length() != word2.length()) {
+            return false;
+        }
+
+        int numberOfDiffs = 0;
+        for (int i=0; i<word1.length(); i++) {
+            if (word1.charAt(i) != word2.charAt(i)) {
+                numberOfDiffs++;
+            }
+        }
+
+        return numberOfDiffs == 1;
+    }
+
+
+    Collection<String> findShortestSolution(String startWord, String endWord) {
+        if ( !graph.nodes().contains(startWord) || !graph.nodes().contains(endWord)) {
+            throw new UnknownWordException();
+        }
+
+        Set<String> visited = new HashSet<>();
+
+        Queue<SolutionNode> wordQueue = new LinkedList<>();
+        wordQueue.add(new SolutionNode(startWord));
+
+        while (!wordQueue.isEmpty()) {
+            SolutionNode processedNode = wordQueue.poll();
+
+            for (String word: getUnvisitedNeighborsOf(processedNode, visited) ) {
+                SolutionNode child = new SolutionNode(processedNode, word);
+                if (child.value.equals(endWord)) {
+                    return child.getFullPath();
+                }
+
+                wordQueue.add(child);
+                visited.add(word);
+            }
+        }
+
+
+        throw new NoSolutionFoundException();
+    }
+
+
+    private Collection<String> getUnvisitedNeighborsOf(SolutionNode word, Collection<String> visited) {
+        return getUnvisitedNeighborsOf(word.value, visited);
+    }
+
+    private Collection<String> getUnvisitedNeighborsOf(String word, Collection<String> visited) {
+        return this.graph
+                .adjacentNodes(word)
+                .stream()
+                .filter(w -> !visited.contains(w))
+                .collect(Collectors.toList());
+    }
+
+
+    private class SolutionNode {
+        final SolutionNode parent;
+        final String value;
+
+        SolutionNode(String value) {
+            this(null, value);
+        }
+
+        SolutionNode(SolutionNode parent, String value) {
+            this.value = value;
+            this.parent = parent;
+        }
+
+        List<String> getFullPath() {
+            List<String> result = new LinkedList<>();
+
+            SolutionNode ptr = this;
+            while (ptr != null) {
+                result.add(0, ptr.value);
+                ptr = ptr.parent;
+            }
+
+            return result;
+        }
+    }
+}
