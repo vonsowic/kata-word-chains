1b08a39cefd919e9f98bcac4b04601bb151df98f
sawim <mwasowicz7@gmail.com>
2018-08-22 21:14:31 +0200

    add default request responses to exceptions

--- src/main/java/com/vonsowic/kata/exceptions/WordsLengthNotEqualException.java
+++ src/main/java/com/vonsowic/kata/exceptions/WordsLengthNotEqualException.java
@@ -1,3 +1,7 @@
 package com.vonsowic.kata.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Words must have the same length")
 public class WordsLengthNotEqualException extends RuntimeException { }
