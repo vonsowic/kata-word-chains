c6adc8f995481c2b82c797c0eeaf03e5dabd2da9
sawim <mwasowicz7@gmail.com>
2018-08-22 22:34:46 +0200

    load selected file

--- src/main/java/com/vonsowic/kata/AppConfiguration.java
+++ src/main/java/com/vonsowic/kata/AppConfiguration.java
@@ -1,6 +1,8 @@
 package com.vonsowic.kata;
 
 import com.vonsowic.kata.katasolver.WordChainSolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +12,25 @@ import java.io.IOException;
 @Configuration
 public class AppConfiguration {
 
+    private static final Logger logger = LoggerFactory.getLogger(AppConfiguration.class);
+
+    private static String wordlistFileName = "wordlist.txt";
+
     @Bean
     public WordChainSolver solver() throws IOException {
+        logger.info("Start of loading wordlist from file: " + wordlistFileName + ". Please be patient.");
         return WordChainSolver
                 .createFromFile(new File(getClass()
                         .getClassLoader()
-                        .getResource("wordlist.txt")
+                        .getResource(getWordlistFileName())
                         .getFile()));
+    }
+
+    public static String getWordlistFileName() {
+        return wordlistFileName;
+    }
+
+    public static void setWordlistFileName(String wordlistFileName) {
+        AppConfiguration.wordlistFileName = wordlistFileName;
     }
 }
